bc3e6e638afac7f529ad536baf4570414ab15f44
florian-ariasu <ariasu.florian@gmail.com>
2026-03-14 19:09:36 +0200

    Change nvim theme to tokyo night storm

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,30 +1,17 @@
 return {
   {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
     opts = {
-      flavour = "macchiato", -- This sets the specific type to Macchiato
-      transparent_background = false, -- Set to true if you want your terminal bg
-      term_colors = true,
-      integrations = {
-        blink_cmp = false, -- Since we disabled blink earlier
-        gitsigns = true,
-        nvimtree = true,
-        treesitter = true,
-        notify = true,
-        mini = {
-          enabled = true,
-          indentscope_color = "",
-        },
-      },
+      style = "storm",
+      transparent = false,
+      terminal_colors = true,
     },
   },
-
-  -- Tell LazyVim to load catppuccin on startup
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "catppuccin",
+      colorscheme = "tokyonight-storm",
     },
   },
 }
