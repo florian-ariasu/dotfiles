c15d9bdad3b638213f34fbc61f345fea08ea58d1
florian-ariasu <ariasu.florian@gmail.com>
2026-06-11 12:34:24 +0300

    Change nvim theme to poimandres

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,17 +1,19 @@
 return {
   {
-    "folke/tokyonight.nvim",
-    name = "tokyonight",
+    "olivercederborg/poimandres.nvim",
+    name = "poimandres",
     opts = {
-      style = "night",
-      transparent = false,
+      bold_vert_split = false,
+      dim_nc_background = false,
+      disable_background = false,
+      disable_float_background = false,
       terminal_colors = true,
     },
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "poimandres",
     },
   },
 }
